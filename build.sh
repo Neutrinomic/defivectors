9d588db82325cc076f8f6fc03e0b0474365813b5
infu <vtsukev@gmail.com>
2024-07-30 15:06:18 +0300

    adding meta in build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-`mocv bin 0.11.3`/moc `mops sources` src/main.mo --idl -o build/main.wasm
+`mocv bin 0.11.3`/moc `mops sources` src/main.mo --idl --public-metadata candid:service --public-metadata candid:args -o build/main.wasm
 didc bind "build/main.did" --target js > "build/main.idl.js"
 didc bind "build/main.did" --target ts > "build/main.idl.d.ts"
   
